4e829183459141a42b98931da81d3ee165950f86
Mathieu Charbonneau <1999146@crosemont.qc.ca>
2021-06-15 23:19:39 -0400

    Implémenter l'authentificaiton

--- javascript/javaScript.js
+++ javascript/javaScript.js
@@ -15,3 +15,15 @@ var loadFile = function (event) {
   var image = document.getElementById(event.target.id + "Img");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+/**
+ * Login
+ */
+function login() {
+  let username = document.getElementById("username");
+  if (username.value == "admin") {
+    window.location.replace("inscription_admin.html");
+  } else {
+    window.location.replace("tableau_bord_parent.html");
+  }
+}
