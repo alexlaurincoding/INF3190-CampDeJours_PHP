ab513db6eb112eddc76ec0289ece29fdda25acd3
Mathieu Charbonneau <1999146@crosemont.qc.ca>
2021-06-29 16:07:20 -0400

    Ajouter un utilitaire de redirection

--- util/Util.php
+++ util/Util.php
@@ -3,4 +3,14 @@ class Util {
     public static function sanitizeUserInput($input) {
         return trim(stripslashes(htmlspecialchars($input)));
     }
+
+    public static function getChemin() {
+    return (ISSET($_SERVER['REDIRECT_BASE'])) ? $_SERVER['REDIRECT_BASE'] : '.';
+    }
+
+    public static function redirectControlleur($controlleur,$action){
+        $url = getChemin()."/$controlleur/$action";
+        header('Location: '.$url);
+        die();
+    }
 }
