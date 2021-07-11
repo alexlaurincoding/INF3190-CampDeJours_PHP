9cbea69ac70ed62c89833671f9f6c1587c1ef5f2
alex laurin <alexlaurincoding@gmail.com>
2021-07-10 22:33:52 -0400

    mofification BD changer id foreingn key dans activite de varchar 30 a varchar 40

--- modele/InscriptionsJSONModel.php
+++ modele/InscriptionsJSONModel.php
@@ -16,7 +16,86 @@ class InscriptionsJSONModel{
         $this->Semaine = $Semaine;
         $this->Payé = $Payé;
     }
+   
+    public function jsonSerialize()
+    {
+        $vars = get_object_vars($this);
 
-    
-    
+        return $vars;
+    }
+   
+    /**
+     * Getter et setters
+     */ 
+    public function getEnfant()
+    {
+        return $this->Enfant;
+    }
+
+    public function setEnfant($Enfant)
+    {
+        $this->Enfant = $Enfant;
+
+        return $this;
+    }
+
+    public function getSession()
+    {
+        return $this->Session;
+    }
+
+    public function setSession($Session)
+    {
+        $this->Session = $Session;
+
+        return $this;
+    }
+
+    public function getParent()
+    {
+        return $this->Parent;
+    }
+
+    public function setParent($Parent)
+    {
+        $this->Parent = $Parent;
+
+        return $this;
+    }
+
+    public function getProgramme()
+    {
+        return $this->Programme;
+    }
+
+    public function setProgramme($Programme)
+    {
+        $this->Programme = $Programme;
+
+        return $this;
+    }
+
+    public function getSemaine()
+    {
+        return $this->Semaine;
+    }
+
+    public function setSemaine($Semaine)
+    {
+        $this->Semaine = $Semaine;
+
+        return $this;
+    }
+
+    public function getPayé()
+    {
+        return $this->Payé;
+    }
+
+    public function setPayé($Payé)
+    {
+        $this->Payé = $Payé;
+
+        return $this;
+    }
 }
