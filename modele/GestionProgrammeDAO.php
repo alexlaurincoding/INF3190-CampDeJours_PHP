cf3e255a698285b5f04644e823b5ea331ab6e569
comtois-etienne <comtois.etienne@github.com>
2021-07-11 21:15:52 -0400

    Revert "Gestion Programme refactor ordre region"
    This reverts commit 15f510ed990d16081c041cf25192e6cb30f067fd.

--- modele/GestionProgrammeDAO.php
+++ modele/GestionProgrammeDAO.php
@@ -1,7 +1,7 @@
 <?php
 class GestionProgrammeDAO {
 
-    #region Session
+    // SESSION
 
     public static function creerSession(SessionModel $session){
         $bdd = BaseDonnee::getConnexion();
@@ -31,6 +31,21 @@ class GestionProgrammeDAO {
         BaseDonnee::close();
     }
 
+    public static function getGabaritsProgramme(){
+        $bdd = BaseDonnee::getConnexion();
+        $gabaritsProgramme = array();
+        $reponse = $bdd->query('SELECT * FROM gabarit_programme');
+        while($donnee = $reponse->fetch()){
+            $gabaritProgramme = new GabaritProgrammeModel($donnee['id'], 
+                                        $donnee['titre'], 
+                                        $donnee['description']);
+            array_push($gabaritsProgramme, $gabaritProgramme);
+        }
+        BaseDonnee::close();       
+
+        return $gabaritsProgramme;
+    }
+
     public static function getSessions() {
         $bdd = BaseDonnee::getConnexion();
         $sessions = array();
@@ -48,43 +63,15 @@ class GestionProgrammeDAO {
         return $sessions;
     }
 
-    #endregion Session
-
-    #region Gabarit Programme
-
-    public static function getGabaritsProgramme(){
-        $bdd = BaseDonnee::getConnexion();
-        $gabaritsProgramme = array();
-        $reponse = $bdd->query('SELECT * FROM gabarit_programme');
-        while($donnee = $reponse->fetch()){
-            $gabaritProgramme = new GabaritProgrammeModel($donnee['id'], 
-                                        $donnee['titre'], 
-                                        $donnee['description']);
-            array_push($gabaritsProgramme, $gabaritProgramme);
-        }
-        BaseDonnee::close();       
-
-        return $gabaritsProgramme;
-    }
-
-    #endregion Gabarit Programme
-
-    #region Programme
-
-
-
-
-    #endregion Programme
-
-    #region Activite
+    // ACTIVITE
 
     public static function creerActivite(ActiviteModel $activite) {
         $bdd = BaseDonnee::getConnexion();
         
         $req = $bdd->prepare('INSERT INTO activite_programme(id) 
-                                VALUES (:id);
-                                INSERT INTO activite(id_activite, nom, type_activite) 
-                                VALUES (:id, :nom, :id_type_activite)');
+                                   VALUES (:id);
+                              INSERT INTO activite(id_activite, nom, type_activite) 
+                                   VALUES (:id, :nom, :id_type_activite)');
         $req->execute(array(
             'id'=> $activite->getId(),
             'nom'=> $activite->getNom(),
@@ -94,39 +81,12 @@ class GestionProgrammeDAO {
         BaseDonnee::close();
     }
 
-    public static function getActivites() {
-        $bdd = BaseDonnee::getConnexion();
-        $activites = array();
-        $req = $bdd->query('SELECT * FROM activite');
-        while($donnee = $req->fetch()){
-            $activite = new ActiviteModel($donnee['id_activite'], 
-                                            $donnee['nom'], 
-                                            $donnee['type_activite']);
-            array_push($activites, $activite);
-        }
-        BaseDonnee::close();
-
-        return $activites;
-    }
-
-    public static function getActivite($id) {
-        $bdd = BaseDonnee::getConnexion();
-
-        $req = $bdd->prepare('SELECT * FROM activite WHERE id_activite = :id');
-        $req->execute(array('id'=>$id));
-        $donnee = $req->fetch();
-        $activite = new ActiviteModel( $donnee['id_activite'], $donnee['nom'], $donnee['type_activite'] );
-
-        BaseDonnee::close();
-        return $activite;
-    }
-
     public static function getActivitesParType($idType) {
         $bdd = BaseDonnee::getConnexion();
         $activites = array();
         $req = $bdd->prepare('SELECT * 
                                 FROM activite
-                                WHERE type_activite = :id_type_activite');
+                               WHERE type_activite = :id_type_activite');
 
         $req->execute(array(
             'id_type_activite' => $idType
@@ -134,8 +94,8 @@ class GestionProgrammeDAO {
 
         while($donnee = $req->fetch()){
             $activite = new ActiviteModel($donnee['id_activite'], 
-                                            $donnee['nom'], 
-                                            $donnee['type_activite']);
+                                          $donnee['nom'], 
+                                          $donnee['type_activite']);
             array_push($activites, $activite);
         }
         BaseDonnee::close();
@@ -143,70 +103,19 @@ class GestionProgrammeDAO {
         return $activites;
     }
 
-    #endregion Activite
-
-    #region Type Activite
-
-    public static function creerTypeActivite(TypeActiviteModel $type) {
+    public static function getActivites() {
         $bdd = BaseDonnee::getConnexion();
-
-        $req = $bdd->prepare('INSERT INTO 
-                        TYPE_ACTIVITE (id, nom, description)
-                            VALUES (:id, :nom, :description)');
-        $req->execute(array(
-            'id' => $type->getId(),
-            'nom' => $type->getNom(),
-            'description' => $type->getDescription(),
-        ));
-
-        $bdd = BaseDonnee::close();
-    }
-
-    public static function getTypesActivite(){
-        $bdd = BaseDonnee::getConnexion();
-
-        $typesActivite = array();
-        $res = $bdd->query('SELECT * FROM type_activite');
-        while($donnee = $res->fetch()){
-            $type = new TypeActiviteModel($donnee['id'], 
-                                    $donnee['nom'], 
-                                    $donnee['description'] 
-                                );
-            array_push($typesActivite, $type);
+        $activites = array();
+        $req = $bdd->query('SELECT * FROM activite');
+        while($donnee = $req->fetch()){
+            $activite = new ActiviteModel($donnee['id_activite'], 
+                                          $donnee['nom'], 
+                                          $donnee['type_activite']);
+            array_push($activites, $activite);
         }
-
-        BaseDonnee::close();       
-
-        return $typesActivite;
-    }
-
-    #endregion Type Activite
-
-    #region Bloc
-
-    public static function creerBlocActivite(BlocModel $blocActivite){
-        $bdd = BaseDonnee::getConnexion();
-        $req = $bdd->prepare('INSERT INTO activite_programme (id) VALUES (:id)');
-        $req->execute(array('id'=> $blocActivite->getId()));
-        
-        $req = $bdd->prepare('INSERT INTO bloc (id_bloc, nom) VALUES (:id_bloc, :nom)');
-        $req->execute(array(
-        'id_bloc' => $blocActivite->getId(),
-        'nom'=> $blocActivite->getNom()
-        )); 
-
-        $activites = $blocActivite->getActivites();
-        $ordre = 0;
-        foreach($activites as $activite){
-            $req = $bdd->prepare('INSERT INTO activite_du_bloc (id_activite, id_bloc, ordre) values (:id_activite, :id_bloc, :ordre)');
-            $req->execute(array(
-                'id_activite'=>$activite->getId(),
-                'id_bloc'=>$blocActivite->getId(),
-                'ordre'=>++$ordre
-            ));
-        }
-
         BaseDonnee::close();
+
+        return $activites;
     }
 
     public static function getBlocsActivite(){
@@ -240,8 +149,114 @@ class GestionProgrammeDAO {
         return $activites;         
     }
 
-    #endregion Bloc
+    public static function getActivite($id) {
+        $bdd = BaseDonnee::getConnexion();
+
+        $req = $bdd->prepare('SELECT * FROM activite WHERE id_activite = :id');
+        $req->execute(array('id'=>$id));
+        $donnee = $req->fetch();
+        $activite = new ActiviteModel( $donnee['id_activite'], $donnee['nom'], $donnee['type_activite'] );
+
+        BaseDonnee::close();
+        return $activite;
+    }
+
+    public static function creerBlocActivite(BlocModel $blocActivite){
+        $bdd = BaseDonnee::getConnexion();
+        $req = $bdd->prepare('INSERT INTO activite_programme (id) VALUES (:id)');
+        $req->execute(array('id'=> $blocActivite->getId()));
+        
+        $req = $bdd->prepare('INSERT INTO bloc (id_bloc, nom) VALUES (:id_bloc, :nom)');
+        $req->execute(array(
+        'id_bloc' => $blocActivite->getId(),
+        'nom'=> $blocActivite->getNom()
+    )); 
+    $activites = $blocActivite->getActivites();
+    $ordre = 0;
+    foreach($activites as $activite){
+        $req = $bdd->prepare('INSERT INTO activite_du_bloc (id_activite, id_bloc, ordre) values (:id_activite, :id_bloc, :ordre)');
+        $req->execute(array(
+            'id_activite'=>$activite->getId(),
+            'id_bloc'=>$blocActivite->getId(),
+            'ordre'=>++$ordre
+        ));
+    }
+
+    BaseDonnee::close();
+
+    }
+
+    // BLOC
+
+    // public static function creerBloc(BlocModel $bloc){
+    //     $bdd = BaseDonnee::getConnexion();
+
+    //     //sauvegarde parent dans la BD            
+    //     $req = $bdd->prepare('INSERT INTO bloc(id, nom, description, date_debut, date_fin) 
+    //                             VALUES (:id, :nom, :description, :date_debut, :date_fin)');
+    //     $req->execute(array(
+    //         'id'=> $session->getId(),
+    //         'nom'=> $session->getNom(),
+    //         'description'=> $session->getDescription(),
+    //         'date_debut'=> $session->getDateDebut(),
+    //         'date_fin'=> $session->getDateFin(),
+    //     )); 
+
+    //     BaseDonnee::close();
+    // }
+
+    // public static function getBlocs(){
+    //     $bdd = BaseDonnee::getConnexion();
+    //     $blocs = array();
+    //     //sauvegarde parent dans la BD            
+    //     $res = $bdd->query('SELECT * FROM bloc');
+    //     while($donnee = $res->fetch()){
+    //         $session = new SessionModel($donnee['id'], 
+    //                                     $donnee['nom'], 
+    //                                     $donnee['description'], 
+    //                                     $donnee['date_debut'], 
+    //                                     $donnee['date_fin']);
+    //         array_push($sessions, $session);
+    //     }
+    //     BaseDonnee::close();       
+
+    //     return $blocs;
+    // }
 
 
+    // TYPE ACTIVITE
+
+    public static function creerTypeActivite(TypeActiviteModel $type) {
+        $bdd = BaseDonnee::getConnexion();
+
+        $req = $bdd->prepare('INSERT INTO 
+                        TYPE_ACTIVITE (id, nom, description)
+                            VALUES (:id, :nom, :description)');
+        $req->execute(array(
+            'id' => $type->getId(),
+            'nom' => $type->getNom(),
+            'description' => $type->getDescription(),
+        ));
+
+        $bdd = BaseDonnee::close();
+    }
+
+    public static function getTypesActivite(){
+        $bdd = BaseDonnee::getConnexion();
+
+        $typesActivite = array();
+        $res = $bdd->query('SELECT * FROM type_activite');
+        while($donnee = $res->fetch()){
+            $type = new TypeActiviteModel($donnee['id'], 
+                                    $donnee['nom'], 
+                                    $donnee['description'] 
+                                );
+            array_push($typesActivite, $type);
+        }
+
+        BaseDonnee::close();       
+
+        return $typesActivite;
+    }
 
 }
